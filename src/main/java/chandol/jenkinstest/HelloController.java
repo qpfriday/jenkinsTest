73f4552671c2607f61ac10b3258e8d786c31fa82
chan <qpfriday@gmail.>
2024-06-01 20:32:30 +0900

    Test

--- src/main/java/chandol/jenkinstest/HelloController.java
+++ src/main/java/chandol/jenkinstest/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String getHello() {
-        return "Hello Jenkins2";
+        return "Hello Jenkins3";
     }
 }
